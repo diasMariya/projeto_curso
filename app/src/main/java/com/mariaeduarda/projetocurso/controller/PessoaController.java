45859383f2b57b8a692806ec481c24fa5ecff231
dudda2130 <mariadiassan147duda@gmail.com>
2025-06-04 14:57:18 -0300

    Testando branch

---
+++ app/src/main/java/com/mariaeduarda/projetocurso/controller/PessoaController.java
@@ -0,0 +1,25 @@
+package com.mariaeduarda.projetocurso.controller;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.mariaeduarda.projetocurso.model.Pessoa;
+
+public class PessoaController {
+
+    @NonNull
+    @Override
+    public String toString() {
+        Log.d("MVC_Controller", "Controller iniciado");
+        return super.toString();
+    }
+
+    public void salvar(Pessoa pessoa) {
+        Log.d("MVC_Controller", "Salvo: " + pessoa.toString());
+    }
+
+    public void limpar(Pessoa pessoa) {
+        Log.d("MVC_Controller", "Salvo: " + pessoa.toString());
+    }
+}
